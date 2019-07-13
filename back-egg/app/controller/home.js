85700a854b1a4579c9327483e64bf0c9d3c4c249
史莱姆 <478889187@qq.com>
2019-07-13 19:05:08 +0800

    docs: 新增调研历史文档

--- back-egg/app/controller/home.js
+++ back-egg/app/controller/home.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const spawn = require('cross-spawn');
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
+    await spawn('npm', [ '-v' ], { stdio: 'inherit' });
   }
 }
 
