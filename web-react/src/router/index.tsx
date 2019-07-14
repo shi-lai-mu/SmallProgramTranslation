a4d2570447acb095a013aba95e322932a66da5f4
史莱姆 <478889187@qq.com>
2019-07-14 19:49:22 +0800

    feat: react router

--- web-react/src/router/index.tsx
+++ web-react/src/router/index.tsx
@@ -1,1 +1,45 @@
+
+import {
+  HashRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import * as React from 'react';
+import Routers from './router';
+
+
+type RouterModel = {
+  path: string,
+  name: string,
+  icon?: string,
+  component?: any,
+  subRoutes?: Array<RouterModel>
+}
+const routerList: Array<RouterModel> = Routers
+console.log(routerList)
+
+class RouterComponent extends React.Component<any, any> {
+  render() {
+    return (
+      <Router>
+        <Switch>
+        {
+          routerList.map((item: RouterModel, index) => {
+            console.log(index)
+            return (
+              <Route
+                path={item.path}
+                exact
+                component={item.component}
+                key={index}
+              />
+            )
+          })
+        }
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default RouterComponent;
---
+++ web-react/src/router/router.tsx
@@ -0,0 +1,12 @@
+
+// import base from '../pages/Bate'
+
+const Routers: Array<any> = [
+  // {
+  //   path: '/bate333',
+  //   name: 'routerBate',
+  //   component: base,
+  // }
+]
+
+export default Routers
