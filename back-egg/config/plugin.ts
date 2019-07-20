5a79e2dc460a5c17af9dcaaade974c8875b74e31
史莱姆 <478889187@qq.com>
2019-07-20 21:04:53 +0800

    chore: 添加mysql、cors

--- back-egg/config/plugin.ts
+++ back-egg/config/plugin.ts
@@ -6,6 +6,14 @@ const plugin: EggPlugin = {
   //   enable: true,
   //   package: 'egg-view-nunjucks',
   // },
+  mysql: {
+    enable: true,
+    package: 'egg-mysql',
+  },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
 };
 
 export default plugin;
