842543f8e9fa7def01e267cb8ffb1c0438ebdd4a
史莱姆 <478889187@qq.com>
2019-07-21 23:04:54 +0800

    feat: 编译测试

--- web-react/src/pages/DesignProduction/ContentBox/ComponentsPool/generalPool/index.tsx
+++ web-react/src/pages/DesignProduction/ContentBox/ComponentsPool/generalPool/index.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
 import { Button } from 'antd';
+import axios from 'axios';
 
 export default class GeneralPool extends React.Component<any, any> {
 
   constructor(props: any) {
     super(props);
+    this.compileTest = this.compileTest.bind(this);
+  }
+
+  compileTest() {
+    axios.get('http://127.0.0.1:7001/test').then(res => {
+      console.log(res.data)
+    })
   }
 
   render () {
     return (
-      <div>
-        <Button type='primary'>测试编译</Button>
+      <div style={{padding: '20px', boxSizing: 'border-box'}}>
+        <Button type='primary' onClick={this.compileTest}>测试编译</Button>
       </div>
     )
   }
