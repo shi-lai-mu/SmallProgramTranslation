2e26478ae1370ddc5409ab97fbbb30fe319d3d13
史莱姆 <478889187@qq.com>
2019-08-25 21:43:47 +0800

    feat: 静态环境 引入声明

--- back-egg/app/pool/loginTest/components/private/index.js
+++ back-egg/app/pool/loginTest/components/private/index.js
@@ -1,48 +1,74 @@
 module.exports = {
   vue: `
-    <div class="select-box">
-        <div class="input-row border">
-            <span class="title">实时测试</span>
-        </div>
-        <div class="input-row">
-            <span class="title">组建池1号编译测试：</span>
-            {{ x + x }}:{{ x }}:{{ x }}.sdsdsd
-            <button @click="s">➕</button>
-        </div>
-        <all :z="x"></all>
-        <test></test>
+    <div class="content">
+      <div class="input-group">
+          <div class="input-row border">
+              <span class="title">账号：</span>
+              <input class="input" type="span" clearable focus v-model="account" placeholder="请输入账号"/>
+          </div>
+          <div class="input-row">
+              <span class="title">密码：</span>
+              <input type="password" displayable v-model="password" placeholder="请输入密码"/>
+          </div>
+      </div>
+      <div class="btn-row">
+          <button type="primary" class="primary" @click="bindLogin">登录</button>
+      </div>
+      <div class="action-row">
+          <a href="../reg/reg">注册账号</a>
+          <span>|</span>
+          <a href="../pwd/pwd">忘记密码</a>
+      </div>
     </div>
   `,
   defaultData: `
-    x: 0,
+    providerList: [],
+    hasProvider: false,
+    account: '',
+    password: '',
+    positionTop: 0,
   `,
   methods: `
-    s () {
-      this.x++;
-    },
-    un() {
-      document.body.removeChild(vue);
-      var script = document.createElement('script');
-      script.id = 'vue';
-      script.src = 'http://127.0.0.1:7001/test.js';
-      document.body.append(script);
+    bindLogin() {
+      /**
+       * 客户端对账号信息进行一些必要的校验。
+       * 实际开发中，根据业务需要进行处理，这里仅做示例。
+       */
+      if (this.account.length < 5) {
+          uni.showToast({
+              icon: 'none',
+              title: '账号最短为 5 个字符'
+          });
+          return;
+      }
+      if (this.password.length < 6) {
+          uni.showToast({
+              icon: 'none',
+              title: '密码最短为 6 个字符'
+          });
+          return;
+      }
+      /**
+       * 下面简单模拟下服务端的处理
+       * 检测用户账号密码是否在已注册的用户列表中
+       * 实际开发中，使用 uni.request 将账号信息发送至服务端，客户端在回调函数中获取结果信息。
+       */
+      const data = {
+          account: this.account,
+          password: this.password
+      };
+      const validUser = service.getUsers().some(function (user) {
+          return data.account === user.account && data.password === user.password;
+      });
+      if (validUser) {
+          this.toMain(this.account);
+      } else {
+          uni.showToast({
+              icon: 'none',
+              title: '用户账号或密码不正确',
+          });
+      }
     },
   `,
-  components: `
-    'test': {
-      data: () => ({
-        msg: 'vue'
-      }),
-      methods: {
-        test() {
-          console.log(this)
-        }
-      },
-      template: '<p @click="test">Hi,{{ msg }}!</p>'
-    },
-    'all': {
-      props: ['z'],
-      template: '<p>这是个子组件 通讯测试：{{ z }}</p>'
-    },
-  `
+  components: ''
 }
